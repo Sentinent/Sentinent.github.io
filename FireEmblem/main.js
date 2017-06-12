ebf345d6b82beccce2f285a66de8885940cf70dc
Sentinent <MonkeyzFtw@gmail.com>
2017-06-12 02:26:18 -0700

    Better choices

--- FireEmblem/main.js
+++ FireEmblem/main.js
@@ -1,13 +1,20 @@
 "use strict";
 
 const things = [
-    "Sucks", "Blows", "is a dumb reroll game", "is for losers", "is terrible", "is garbage", "Stinks", "is a sorry excuse for a game", "is literally worse than Enigma", "deserves to be deleted", "is only played because of Nowi", "is for idiots who can't play real games"
+    "Sucks", "Blows", "is a dumb reroll game", "is for losers", "is terrible", "is garbage", "Stinks", "is a sorry excuse for a game", "is literally worse than Enigma", "deserves to be deleted", "is only played because of Nowi"
 ];
 
-var elem = document.getElementById("desc");
+const elem = document.getElementById("desc");
+var lastChoice = undefined;
 
 document.addEventListener("click", function () {
-    elem.innerHTML = things[Math.floor(Math.random() * things.length)];
+    var choice = things[Math.floor(Math.random() * things.length)];
+    while (choice === lastChoice) {
+        choice = things[Math.floor(Math.random() * things.length)];
+    }
+    lastChoice = choice;
+    
+    elem.innerHTML = choice;
     
     elem.classList.remove("animate");
     void elem.offsetWidth;
